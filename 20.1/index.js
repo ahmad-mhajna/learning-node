998d96d8b767d963eabe42fba0df1318a0dff558
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-06-21 20:35:49 +0300

    removed update from 20

--- 20.1/index.js
+++ 20.1/index.js
@@ -33,25 +33,6 @@ app.delete("/products/deleteAll", async (req, res) => {
     res.status(500).send(e);
   }
 });
-app.put("/products/update/", async (req, res) => {
-  const id = req.body.id;
-  const isActive = req.body.isActive;
-  console.log(typeof isActive);
-  try {
-    if (typeof isActive !== "boolean") {
-      return res.status(400).send("it should be true or false");
-    }
-    const product = await Product.findById(id);
-    if (!product) {
-      return res.status(404).send("this item dose not exist");
-    }
-    product.isActive = isActive;
-    await product.save();
-    res.send(product);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
 app.listen(port, () => {
   console.log("server is up " + port);
 });
